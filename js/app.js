7ce4b0a594bd19292f0f988422ec398488481850
TinySkillet <mirageaditya@gmail.com>
2024-06-10 22:00:55 +0545

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplayspeed: 1000,
+        appendDots: '.slider-dots',
+        dotsClass: 'dots'
+    })
+})
